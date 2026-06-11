3495200ef0c8f3d6976a1316d4826d528b44e9ca
Ondrej Mirtes <ondrej@mirtes.cz>
2026-06-11 12:36:11 +0200

    ExpressionResult - add Expr

--- src/Analyser/ExprHandler/BooleanOrHandler.php
+++ src/Analyser/ExprHandler/BooleanOrHandler.php
@@ -354,6 +354,7 @@ final class BooleanOrHandler implements ExprHandler
 		return $this->expressionResultFactory->create(
 			$leftMergedWithRightScope,
 			beforeScope: $scope,
+			expr: $expr,
 			hasYield: $leftResult->hasYield() || $rightResult->hasYield(),
 			isAlwaysTerminating: $leftResult->isAlwaysTerminating(),
 			throwPoints: array_merge($leftResult->getThrowPoints(), $rightResult->getThrowPoints()),
