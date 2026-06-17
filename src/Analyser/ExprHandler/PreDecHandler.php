b352f0b94fc5d8eb4a614b93c4671defe5f56b15
Ondrej Mirtes <ondrej@mirtes.cz>
2026-06-17 15:29:41 +0200

    PreDecHandler is no longer TypeResolvingExprHandler

--- src/Analyser/ExprHandler/PreDecHandler.php
+++ src/Analyser/ExprHandler/PreDecHandler.php
@@ -3,7 +3,6 @@
 namespace PHPStan\Analyser\ExprHandler;
 
 use PhpParser\Node\Expr;
-use PhpParser\Node\Expr\BinaryOp\Minus;
 use PhpParser\Node\Expr\PreDec;
 use PhpParser\Node\Scalar\Int_;
 use PhpParser\Node\Stmt;
@@ -11,16 +10,18 @@ use PHPStan\Analyser\ExpressionContext;
 use PHPStan\Analyser\ExpressionResult;
 use PHPStan\Analyser\ExpressionResultFactory;
 use PHPStan\Analyser\ExpressionResultStorage;
+use PHPStan\Analyser\ExprHandler;
+use PHPStan\Analyser\ExprHandler\Helper\DefaultNarrowingHelper;
 use PHPStan\Analyser\MutatingScope;
 use PHPStan\Analyser\NodeScopeResolver;
-use PHPStan\Analyser\Scope;
 use PHPStan\Analyser\SpecifiedTypes;
-use PHPStan\Analyser\TypeResolvingExprHandler;
-use PHPStan\Analyser\TypeSpecifier;
 use PHPStan\Analyser\TypeSpecifierContext;
 use PHPStan\DependencyInjection\AutowiredService;
+use PHPStan\Reflection\InitializerExprTypeResolver;
+use PHPStan\ShouldNotHappenException;
 use PHPStan\Type\Accessory\AccessoryLiteralStringType;
 use PHPStan\Type\BenevolentUnionType;
+use PHPStan\Type\Constant\ConstantIntegerType;
 use PHPStan\Type\FloatType;
 use PHPStan\Type\IntegerType;
 use PHPStan\Type\IntersectionType;
@@ -35,13 +36,17 @@ use function is_string;
 use function str_decrement;
 
 /**
- * @implements TypeResolvingExprHandler<PreDec>
+ * @implements ExprHandler<PreDec>
  */
 #[AutowiredService]
-final class PreDecHandler implements TypeResolvingExprHandler
+final class PreDecHandler implements ExprHandler
 {
 
-	public function __construct(private ExpressionResultFactory $expressionResultFactory)
+	public function __construct(
+		private ExpressionResultFactory $expressionResultFactory,
+		private InitializerExprTypeResolver $initializerExprTypeResolver,
+		private DefaultNarrowingHelper $defaultNarrowingHelper,
+	)
 	{
 	}
 
@@ -50,58 +55,83 @@ final class PreDecHandler implements TypeResolvingExprHandler
 		return $expr instanceof PreDec;
 	}
 
-	public function resolveType(MutatingScope $scope, Expr $expr): Type
+	public function processExpr(NodeScopeResolver $nodeScopeResolver, Stmt $stmt, Expr $expr, MutatingScope $scope, ExpressionResultStorage $storage, callable $nodeCallback, ExpressionContext $context): ExpressionResult
 	{
-		$varType = $scope->getType($expr->var);
-		$varScalars = $varType->getConstantScalarValues();
+		$varResult = $nodeScopeResolver->processExprNode($stmt, $expr->var, $scope, $storage, $nodeCallback, $context->enterDeep());
 
-		if (count($varScalars) > 0) {
-			$newTypes = [];
+		$typeCallback = function (MutatingScope $s) use ($expr, $varResult): Type {
+			$varType = $varResult->getTypeForScope($s);
+			$varScalars = $varType->getConstantScalarValues();
 
-			foreach ($varScalars as $varValue) {
-				if ($varValue === '') {
-					$varValue = -1;
-				} elseif (is_string($varValue) && !is_numeric($varValue)) {
-					try {
-						$varValue = str_decrement($varValue);
-					} catch (ValueError) {
-						return new NeverType();
+			if (count($varScalars) > 0) {
+				$newTypes = [];
+
+				foreach ($varScalars as $varValue) {
+					if ($varValue === '') {
+						$varValue = -1;
+					} elseif (is_string($varValue) && !is_numeric($varValue)) {
+						try {
+							$varValue = str_decrement($varValue);
+						} catch (ValueError) {
+							return new NeverType();
+						}
+					} elseif (is_numeric($varValue)) {
+						--$varValue;
 					}
-				} elseif (is_numeric($varValue)) {
-					--$varValue;
+
+					$newTypes[] = $s->getTypeFromValue($varValue);
+				}
+				return TypeCombinator::union(...$newTypes);
+			} elseif ($varType->isString()->yes()) {
+				if ($varType->isLiteralString()->yes()) {
+					return new IntersectionType([
+						new StringType(),
+						new AccessoryLiteralStringType(),
+					]);
 				}
 
-				$newTypes[] = $scope->getTypeFromValue($varValue);
-			}
-			return TypeCombinator::union(...$newTypes);
-		} elseif ($varType->isString()->yes()) {
-			if ($varType->isLiteralString()->yes()) {
-				return new IntersectionType([
-					new StringType(),
-					new AccessoryLiteralStringType(),
-				]);
-			}
+				if ($varType->isNumericString()->yes()) {
+					return new BenevolentUnionType([
+						new IntegerType(),
+						new FloatType(),
+					]);
+				}
 
-			if ($varType->isNumericString()->yes()) {
 				return new BenevolentUnionType([
+					new StringType(),
 					new IntegerType(),
 					new FloatType(),
 				]);
 			}
 
-			return new BenevolentUnionType([
-				new StringType(),
-				new IntegerType(),
-				new FloatType(),
-			]);
-		}
+			$one = new Int_(1);
+			return $this->initializerExprTypeResolver->getMinusType($expr->var, $one, static function (Expr $e) use ($s, $expr, $varResult, $one): Type {
+				if ($e === $expr->var) {
+					return $varResult->getTypeForScope($s);
+				}
+				if ($e === $one) {
+					return new ConstantIntegerType(1);
+				}
 
-		return $scope->getType(new Minus($expr->var, new Int_(1)));
-	}
+				throw new ShouldNotHappenException();
+			});
+		};
+		$specifyTypesCallback = fn (MutatingScope $s, TypeSpecifierContext $context): SpecifiedTypes => $this->defaultNarrowingHelper->specifyDefaultTypes($expr, $context);
 
-	public function processExpr(NodeScopeResolver $nodeScopeResolver, Stmt $stmt, Expr $expr, MutatingScope $scope, ExpressionResultStorage $storage, callable $nodeCallback, ExpressionContext $context): ExpressionResult
-	{
-		$varResult = $nodeScopeResolver->processExprNode($stmt, $expr->var, $scope, $storage, $nodeCallback, $context->enterDeep());
+		// processVirtualAssign asks getType($expr) for the value to assign; store
+		// this result first so that resolves from the typeCallback below rather
+		// than re-processing the node on demand (which would recurse).
+		$nodeScopeResolver->storeExpressionResult($storage, $expr, $this->expressionResultFactory->create(
+			$varResult->getScope(),
+			beforeScope: $scope,
+			expr: $expr,
+			hasYield: $varResult->hasYield(),
+			isAlwaysTerminating: $varResult->isAlwaysTerminating(),
+			throwPoints: $varResult->getThrowPoints(),
+			impurePoints: $varResult->getImpurePoints(),
+			typeCallback: $typeCallback,
+			specifyTypesCallback: $specifyTypesCallback,
+		));
 
 		return $this->expressionResultFactory->create(
 			$nodeScopeResolver->processVirtualAssign(
@@ -118,12 +148,9 @@ final class PreDecHandler implements TypeResolvingExprHandler
 			isAlwaysTerminating: $varResult->isAlwaysTerminating(),
 			throwPoints: $varResult->getThrowPoints(),
 			impurePoints: $varResult->getImpurePoints(),
+			typeCallback: $typeCallback,
+			specifyTypesCallback: $specifyTypesCallback,
 		);
-	}
-
-	public function specifyTypes(TypeSpecifier $typeSpecifier, Scope $scope, Expr $expr, TypeSpecifierContext $context): SpecifiedTypes
-	{
-		return $typeSpecifier->specifyDefaultTypes($scope, $expr, $context);
 	}
 
 }
