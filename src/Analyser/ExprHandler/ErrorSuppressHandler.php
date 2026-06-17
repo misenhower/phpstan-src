c9ebf8f747431397bb5da600c5289e013d0a15e3
Ondrej Mirtes <ondrej@mirtes.cz>
2026-06-17 14:07:43 +0200

    ErrorSuppressHandler is no longer TypeResolvingExprHandler

--- src/Analyser/ExprHandler/ErrorSuppressHandler.php
+++ src/Analyser/ExprHandler/ErrorSuppressHandler.php
@@ -9,24 +9,26 @@ use PHPStan\Analyser\ExpressionContext;
 use PHPStan\Analyser\ExpressionResult;
 use PHPStan\Analyser\ExpressionResultFactory;
 use PHPStan\Analyser\ExpressionResultStorage;
+use PHPStan\Analyser\ExprHandler;
+use PHPStan\Analyser\ExprHandler\Helper\DefaultNarrowingHelper;
 use PHPStan\Analyser\MutatingScope;
 use PHPStan\Analyser\NodeScopeResolver;
-use PHPStan\Analyser\Scope;
 use PHPStan\Analyser\SpecifiedTypes;
-use PHPStan\Analyser\TypeResolvingExprHandler;
-use PHPStan\Analyser\TypeSpecifier;
 use PHPStan\Analyser\TypeSpecifierContext;
 use PHPStan\DependencyInjection\AutowiredService;
 use PHPStan\Type\Type;
 
 /**
- * @implements TypeResolvingExprHandler<ErrorSuppress>
+ * @implements ExprHandler<ErrorSuppress>
  */
 #[AutowiredService]
-final class ErrorSuppressHandler implements TypeResolvingExprHandler
+final class ErrorSuppressHandler implements ExprHandler
 {
 
-	public function __construct(private ExpressionResultFactory $expressionResultFactory)
+	public function __construct(
+		private ExpressionResultFactory $expressionResultFactory,
+		private DefaultNarrowingHelper $defaultNarrowingHelper,
+	)
 	{
 	}
 
@@ -37,29 +39,20 @@ final class ErrorSuppressHandler implements TypeResolvingExprHandler
 
 	public function processExpr(NodeScopeResolver $nodeScopeResolver, Stmt $stmt, Expr $expr, MutatingScope $scope, ExpressionResultStorage $storage, callable $nodeCallback, ExpressionContext $context): ExpressionResult
 	{
+		$beforeScope = $scope;
 		$exprResult = $nodeScopeResolver->processExprNode($stmt, $expr->expr, $scope, $storage, $nodeCallback, $context);
 
 		return $this->expressionResultFactory->create(
 			$exprResult->getScope(),
-			beforeScope: $scope,
+			beforeScope: $beforeScope,
 			expr: $expr,
 			hasYield: $exprResult->hasYield(),
 			isAlwaysTerminating: $exprResult->isAlwaysTerminating(),
 			throwPoints: $exprResult->getThrowPoints(),
 			impurePoints: $exprResult->getImpurePoints(),
-			truthyScopeCallback: static fn (): MutatingScope => $exprResult->getTruthyScope(),
-			falseyScopeCallback: static fn (): MutatingScope => $exprResult->getFalseyScope(),
+			typeCallback: static fn (MutatingScope $s): Type => $exprResult->getTypeForScope($s),
+			specifyTypesCallback: fn (MutatingScope $s, TypeSpecifierContext $context): SpecifiedTypes => $this->defaultNarrowingHelper->getChildSpecifiedTypes($s, $expr->expr, $exprResult, $context)->setRootExpr($expr),
 		);
-	}
-
-	public function resolveType(MutatingScope $scope, Expr $expr): Type
-	{
-		return $scope->getType($expr->expr);
-	}
-
-	public function specifyTypes(TypeSpecifier $typeSpecifier, Scope $scope, Expr $expr, TypeSpecifierContext $context): SpecifiedTypes
-	{
-		return $typeSpecifier->specifyTypesInCondition($scope, $expr->expr, $context)->setRootExpr($expr);
 	}
 
 }
