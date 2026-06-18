6bfa8509812517b3e891b6d4a60430429df77425
Ondrej Mirtes <ondrej@mirtes.cz>
2026-06-18 08:08:29 +0200

    Eliminate the GetOffsetValueTypeExpr virtual node

--- src/Analyser/ExprHandler/AssignHandler.php
+++ src/Analyser/ExprHandler/AssignHandler.php
@@ -41,7 +41,6 @@ use PHPStan\Analyser\TypeSpecifier;
 use PHPStan\Analyser\TypeSpecifierContext;
 use PHPStan\DependencyInjection\AutowiredService;
 use PHPStan\Node\Expr\ExistingArrayDimFetch;
-use PHPStan\Node\Expr\GetOffsetValueTypeExpr;
 use PHPStan\Node\Expr\IntertwinedVariableByReferenceWithExpr;
 use PHPStan\Node\Expr\SetExistingOffsetValueTypeExpr;
 use PHPStan\Node\Expr\SetOffsetValueTypeExpr;
@@ -988,7 +987,7 @@ final class AssignHandler implements ExprHandler
 				} else {
 					$dimExpr = $arrayItem->key;
 				}
-				$getOffsetValueTypeExpr = new GetOffsetValueTypeExpr($assignedExpr, $dimExpr);
+				$getOffsetValueTypeExpr = new TypeExpr($scope->getType($assignedExpr)->getOffsetValueType($scope->getType($dimExpr)));
 				$result = $this->processAssignVar(
 					$nodeScopeResolver,
 					$scope,
