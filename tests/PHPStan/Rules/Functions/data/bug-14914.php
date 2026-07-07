23b499dc3ba1e4bed3c327c83d8fe6eea0e0cd52
Ondrej Mirtes <ondrej@mirtes.cz>
2026-07-07 19:33:35 +0200

    Add regression test for #14914

---
+++ tests/PHPStan/Rules/Functions/data/bug-14914.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace Bug14914;
+
+function doFoo(): void
+{
+	preg_replace_callback(
+		'/a|(?<b>b)/',
+		function (array $match): string {
+			if ($match['b'] !== null) {
+				return 'aa';
+			}
+			return 'possible?';
+		},
+		'abcd',
+		flags: PREG_UNMATCHED_AS_NULL,
+	);
+}
+
+function doFoo2(): void
+{
+	preg_replace_callback(
+		'/a|(?<b>b)/',
+		fn (array $match) => $match['b'] !== null ? 'aa' : 'possible?',
+		'abcd',
+		flags: PREG_UNMATCHED_AS_NULL,
+	);
+}
